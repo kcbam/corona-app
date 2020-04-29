fd9225dc20e153c6b132415a8e652dbd59317240
Bam Kadayat <bamkadayat@Bams-MacBook-Air.local>
2020-04-29 11:28:51 +0200

    added useState and useEffect in chart coponent and added some style for cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,34 +1,61 @@
 import React from 'react';
-import styles from './Cards.module.css';
 import { Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CountUp from 'react-countup';
+import styles from './Cards.module.css';
+import cx from 'classnames';
 
 
-const Cards = (props) => {
-
+const Cards = ({ data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    console.log(confirmed);
+    if(!confirmed){
+        return 'Loading..';
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                     <CardContent>
                         <Typography color="text-secondary" gutterBottom> Infected</Typography>
-                        <Typography variant="h5"> REAL DATA</Typography>
-                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={confirmed.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+
+                        <Typography color="text-secondary"> { new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2"> Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
                     <CardContent>
                         <Typography color="text-secondary" gutterBottom> Recovered</Typography>
-                        <Typography variant="h5"> REAL DATA</Typography>
-                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={recovered.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="text-secondary"> { new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2"> Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
-                <Grid item component={Card}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
                     <CardContent>
                         <Typography color="text-secondary" gutterBottom> Deaths</Typography>
-                        <Typography variant="h5"> REAL DATA</Typography>
-                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="h5"> 
+                            <CountUp
+                                start={0}
+                                end={deaths.value}
+                                duration={2.5}
+                                separator=","
+                            />
+                        </Typography>
+                        <Typography color="text-secondary"> { new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2"> Number of deaths cause by COVID-19</Typography>
                     </CardContent>
                 </Grid>
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({});
+    useEffect(() =>{
+        const fetchDailyData = async () =>{
+            const dailyData = await fetchDailyData();
+        }
+    })
     return (
         <h1>Chart</h1>
     );
