07010e19f09833ad13ea453758b67328d6836024
Bam Kadayat <bamkadayat@Bams-MacBook-Air.local>
2020-04-29 10:57:03 +0200

    added material ui to the cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import styles from './Cards.module.css';
+import { Card, CardContent, Typography, Grid} from '@material-ui/core';
 
-const Cards = ({data}) => {
-    
+
+const Cards = (props) => {
+
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="text-secondary" gutterBottom> Infected</Typography>
+                        <Typography variant="h5"> REAL DATA</Typography>
+                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="body2"> Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="text-secondary" gutterBottom> Recovered</Typography>
+                        <Typography variant="h5"> REAL DATA</Typography>
+                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="body2"> Number of recoveries from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="text-secondary" gutterBottom> Deaths</Typography>
+                        <Typography variant="h5"> REAL DATA</Typography>
+                        <Typography color="text-secondary"> REAL DATE</Typography>
+                        <Typography variant="body2"> Number of deaths cause by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+
+        </div>
     );
 }
 export default Cards;
